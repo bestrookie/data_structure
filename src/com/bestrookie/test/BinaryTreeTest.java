fb24ccedc6c9285ab3a9cb2e5e97ac12a3115673
ZR21070043-尹文龙 <yinwenlong@zhongruigroup.com>
2022-05-24 19:34:42 +0800

    二叉查找树完整

---
+++ src/com/bestrookie/test/BinaryTreeTest.java
@@ -0,0 +1,23 @@
+package com.bestrookie.test;
+
+import com.bestrookie.tree.BinaryTree;
+
+import java.util.Objects;
+
+/**
+ * @author bestrookie
+ * @date 2022/5/24 14:55
+ */
+public class BinaryTreeTest {
+    public static void main(String[] args) {
+        BinaryTree<Integer,String> tree = new BinaryTree<>();
+
+        tree.put(1,"一");
+        tree.put(2,"二");
+        tree.put(3,"三");
+        System.out.println("元素个数:"+ tree.size());
+        System.out.println("第二个元素:"+ tree.get(2));
+        tree.delete(1);
+        System.out.println(tree.get(2));
+    }
+}
--- src/com/bestrookie/tree/BinaryTree.java
+++ src/com/bestrookie/tree/BinaryTree.java
@@ -14,19 +14,21 @@ public class BinaryTree <Key extends Comparable<Key>,Value>{
     }
 
     public void put(Key key,Value value){
-
+       root =  put(root,key,value);
     }
 
     public TreeNode put(TreeNode x,Key key,Value value){
         if (x == null){
+            N++;
             return new TreeNode(key,value,null,null);
         }else if (x.key.compareTo(key) > 0){
-            x.right = put(x,key,value);
+            x.left = put(x.left,key,value);
         }else if (x.key.compareTo(key) < 0){
-            x.left = put(x,key,value);
+            x.right = put(x.right,key,value);
         }else {
             x.value = value;
         }
+
         return x;
     }
 
@@ -38,13 +40,89 @@ public class BinaryTree <Key extends Comparable<Key>,Value>{
         if (x == null){
             return null;
         }else if (x.key.compareTo(key) > 0){
-            return get(x.right,key);
-        }else if (x.key.compareTo(key) < 0){
             return get(x.left,key);
+        }else if (x.key.compareTo(key) < 0){
+            return get(x.right,key);
         }else {
             return x.value;
         }
+    }
 
+    public void delete(Key key){
+        delete(root,key);
+    }
+
+    public TreeNode delete(TreeNode x ,Key key){
+        if (x == null){
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp > 0){
+            x.right = delete(x.right,key);
+        }else if (cmp < 0){
+            x.left = delete(x.left,key);
+        }else {
+            N--;
+            if (x.right == null){
+                if (x == root){
+                    root = x.left;
+                }
+                return x.left;
+            }
+            if (x.left == null){
+                if (x == root){
+                    root = x.right;
+                }
+                return x.right;
+            }
+            TreeNode mindNode = x.right;
+            while (mindNode.left != null){
+                mindNode = mindNode.left;
+            }
+
+            TreeNode n = x.right;
+            while (n.left != null){
+                if (n.left.left == null){
+                    if (n.left.right != null){
+                        n.left = n.left.right;
+                    }else {
+                        n.left = null;
+                    }
+                }else {
+                    n = n.left;
+                }
+            }
+
+            mindNode.left = x.left;
+            mindNode.right = x.right;
+            x = mindNode;
+
+        }
+        return x;
+    }
+
+    public Value min(){
+        return min(root).value;
+    }
+
+    public TreeNode min(TreeNode n){
+        if (n.left != null){
+            return min(n.left);
+        }else {
+            return n;
+        }
+    }
+
+    public Value max(){
+        return max(root).value;
+    }
+    public TreeNode max(TreeNode n){
+        if (n.right != null){
+            return max(n.right);
+        }else {
+            return n;
+        }
     }
 
 
