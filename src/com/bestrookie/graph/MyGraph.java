2fd86999a8b43a733fe521cb37d23cee2b52c8af
bestrookie <491051587@qq.com>
2022-09-03 13:25:26 +0800

    无向图

--- src/com/bestrookie/graph/MyGraph.java
+++ src/com/bestrookie/graph/MyGraph.java
@@ -1,6 +1,56 @@
-package com.bestrookie.graph;/**
- * @author  bestrookie
- * @date  2022/9/1 22:20
+package com.bestrookie.graph;
+
+import com.bestrookie.queue.MyQueue;
+
+/**
+ * @author bestrookie
  * @version 1.0
- */public class Graph {
+ * @date 2022/9/1 22:20
+ */
+public class MyGraph {
+    //记录顶点的数量
+    private final int V;
+    //记录边的数量
+    private int E;
+    //邻接表
+    private MyQueue<Integer>[]adj;
+    MyGraph(int v){
+        this.V = v;
+        this.E = 0;
+        this.adj = new MyQueue[v];
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new MyQueue<Integer>();
+        }
+    }
+
+    /**
+     * 获取顶点的数量
+     * @return 顶点数量
+     */
+    public int V(){
+        return V;
+    }
+
+    /**
+     * 获取边的数量
+     * @return 边的数量
+     */
+    private int E(){
+        return E;
+    }
+
+    public void addEdge(int v, int w){
+        adj[v].enQueue(w);
+        adj[w].enQueue(v);
+        E++;
+    }
+
+    /**
+     * 获取某个顶点的边
+     * @param v 顶点
+     * @return 边
+     */
+    public MyQueue<Integer> adj(int v){
+        return adj[v];
+    }
 }
--- src/com/bestrookie/uf/Traffic_Project_Test.java
+++ src/com/bestrookie/uf/Traffic_Project_Test.java
@@ -1,6 +1,28 @@
-package com.bestrookie.uf;/**
- * @author  bestrookie
- * @date  2022/9/1 21:08
+package com.bestrookie.uf;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author bestrookie
  * @version 1.0
- */public class Traffic_Project_Test {
+ * @date 2022/9/1 21:08
+ */
+public class Traffic_Project_Test {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(Traffic_Project_Test.class.getClassLoader().getResourceAsStream("traffic_project.txt")));
+        
+        int totalCity = Integer.parseInt(br.readLine());
+        UF_Tree_Weighted uf = new UF_Tree_Weighted(totalCity);
+        int roadNumber = Integer.parseInt(br.readLine());
+        for (int i = 0; i < roadNumber; i++) {
+            String line = br.readLine();
+            String[] str = line.split(" ");
+            int p = Integer.parseInt(str[0]);
+            int q = Integer.parseInt(str[1]);
+            uf.union(p, q);
+        }
+        System.out.println(uf.count() -1);
+
+    }
 }
